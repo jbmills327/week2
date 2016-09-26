a65f46f2788ed2d810b6bf97d3faad5ce11403d6
justin mills <jbmills@justins-MacBook-Pro.local>
2016-09-26 15:57:36 -0600

    Volunteers added functions

--- victims-and-volunteers/main.js
+++ victims-and-volunteers/main.js
@@ -1,28 +1,51 @@
 var howMany = Number(prompt("How many victims do you have?"));
-var victims = [];
+// var victims = [];
 
-// while(moreToAdd){
-for (var i = 0; i < howMany; i++) {
-    var help = {};
-    help.name = (prompt("What is the name of this victim"));
-    help.street = (prompt("What is the street"));
-    help.phoneNumber = (prompt("What is the phone number"));
-    victims.push(help);
+function needSomeone() {
+    var victims = [];
+    for (var i = 0; i < howMany; i++) {
+        var help = {};
+        help.name = (prompt("What is the name of this victim?"));
+        help.street = (prompt("What is the street?"));
+        help.phoneNumber = (prompt("What is the phone number?"));
+        victims.push(help);
+    }
+    return victims;
 }
 
+console.log(needSomeone());
 
-var numberOfVolunteers = +(prompt("How many rescuers are available?"));
-var volunteers = [];
 
-for (var i = 0; i < numberOfVolunteers; i++) {
-    var help = {};
-    help.name = (prompt("What is the name of this volunteer?"));
-    help.street = (prompt("What street does this volunteer live on?"));
-    help.phoneNumber = (prompt("What is the phone number for this volunteer"));
-    volunteers.push(help);
+
+// needSomeone();
+
+
+var numberOfVolunteers = +(prompt("How many volunteers are available?"));
+// var volunteers = [];
+
+function hereToHelp() {
+    var helpers = [];
+    for (var i = 0; i < numberOfVolunteers; i++) {
+        var help = {};
+        help.name = (prompt("What is the name of this volunteers?"));
+        help.street = (prompt("What street does this volunteer live on?"));
+        help.phoneNumber = (prompt("What is the volunteers phone number?"));
+        helpers.push(help);
+    }
+    return helpers;
 }
 
+console.log(hereToHelp());
+//
+// for (var i = 0; i < numberOfVolunteers; i++) {
+//     var help = {};
+//     help.name = (prompt("What is the name of this volunteer?"));
+//     help.street = (prompt("What street does this volunteer live on?"));
+//     help.phoneNumber = (prompt("What is the phone number for this volunteer"));
+//     volunteers.push(help);
+// }
 
 
-console.log(victims);
-console.log(volunteers);
+
+// console.log(victims);
+// console.log(volunteers);
