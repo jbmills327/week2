a6f81e82bb2994d0b5eabf6a8ec7db329eceefe2
justin mills <jbmills@justins-MacBook-Pro.local>
2016-09-26 14:51:31 -0600

    Volunteers Done

--- victims-and-volunteers/main.js
+++ victims-and-volunteers/main.js
@@ -1,22 +1,28 @@
 var howMany = Number(prompt("How many victims do you have?"));
 var victims = [];
-var help = {};
 
+// while(moreToAdd){
 for (var i = 0; i < howMany; i++) {
+    var help = {};
     help.name = (prompt("What is the name of this victim"));
     help.street = (prompt("What is the street"));
     help.phoneNumber = (prompt("What is the phone number"));
     victims.push(help);
 }
 
-console.log(victims);
 
-// function victims() {
-//     for (i = 0; i < arguments.length; i++) {
-//         victim.name = prompt("What is this victims name?"),
-//             victim.street = prompt("What is this victims street"),
-//             victim.phoneNumber = prompt("What is this victims phone number")
-//     }
-// }
-//
-// victims("Tom", "Ed", "Rick");
+var numberOfVolunteers = +(prompt("How many rescuers are available?"));
+var volunteers = [];
+
+for (var i = 0; i < numberOfVolunteers; i++) {
+    var help = {};
+    help.name = (prompt("What is the name of this volunteer?"));
+    help.street = (prompt("What street does this volunteer live on?"));
+    help.phoneNumber = (prompt("What is the phone number for this volunteer"));
+    volunteers.push(help);
+}
+
+
+
+console.log(victims);
+console.log(volunteers);
